0912668a03989b96987735982f79b8afe24e5e89
TatjanaPro <tatjaana.pronina@gmail.com>
2023-09-01 23:34:02 +0300

    arr test

--- src/Lesson20/ArrTest.java
+++ src/Lesson20/ArrTest.java
@@ -1,31 +1,33 @@
 package Lesson20;
+//{1, 9, 0, 5}
 
-public class ArrLesson {
+public class ArrTest {
 
     public static int[] sortirovka(int[] arr) {
         int a;
         for (int i = 0; i < arr.length; i++) {
-            int min = arr[i]; //arr[0] = 15
-            int index = i; // index = 0
+            int min = arr[i];
+            int index = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < min) {
-                min = arr[j]; // min = arr[1] = 3
-                index = j; //index = 1
+                    min = arr[j];
+                    index = j;
+                }
             }
-        }
             if (i != index) {
-                a = arr[i]; //a = 15
-                arr[i] = min; //arr[0] = 3
-                arr[index] = a; //arr[1] = 15
+                a = arr[i]; //1
+                arr[i] = min; //0
+                arr[index] = a;
             }
         }
         return arr;
     }
     public static void main(String[] args) {
-        int [] arr = {1, 3, 5, 8};
+        int [] arr = {15, 3, 5, 8, 4, 2};
         sortirovka(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
+
 }
